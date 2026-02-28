dd55729c016763fe3e72940c20f1859d6b7a05fb
1ZandandMe <shibashanyiao@163.com>
2026-02-28 16:09:29 +0800

    s

---
+++ build/CMakeFiles/calc_warrior.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc_warrior.dir/code/main.cpp.obj"
+  "CMakeFiles/calc_warrior.dir/code/main.cpp.obj.d"
+  "calc_warrior.exe"
+  "calc_warrior.exe.manifest"
+  "calc_warrior.pdb"
+  "libcalc_warrior.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calc_warrior.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    system("chcp 65001");
+    cout << "CALC_WARRIOR项目配置成功！" << endl;
+    return 0;
+}
