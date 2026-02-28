fc93f8b75a3788e805fc1b7868b1fc9fb4eaccfd
1ZandandMe <shibashanyiao@163.com>
2026-02-28 15:02:47 +0800

    a啊啊

--- code/sin.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-main (){
-    int a = 1;
-    cout << a; 
-    return 0;
-}
--- code/ss.cpp
+++ code/ss.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(){
     int aaa = 1;
-    cout<< aaa ;
+    cout << aaa ;
     return 0;
 }
