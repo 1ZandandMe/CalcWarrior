ba01565d8cfd05ed035b52045fdf22e805d2d393
1ZandandMe <shibashanyiao@163.com>
2026-04-05 18:12:02 +0800

    ``

--- pycode/shi.py
+++
@@ -1,9 +0,0 @@
-a = ["kal","akl ","s","shis","kal"]
-
-count_kal = a.count("kal")
-# print(f"'kal' appears {count_kal} times")
-
-# Count all items in the list
-for item in a:
-    print(f"'{item}' appears {a.count(item)} times")
-"""上面是"""
